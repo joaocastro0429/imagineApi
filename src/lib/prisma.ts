933f1fdcca0fe41216cdddbf3230f416159875c3
joaocastro <joaocastro0429@gmail.com>
2024-08-04 18:03:36 -0300

    modelando o banco de dados

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import {  PrismaClient } from "@prisma/client";
+import { PrismaClient} from '@prisma/client';
 
-export const prisma= new PrismaClient()
-prisma.user
+export const prisma = new PrismaClient();
+
